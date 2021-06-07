f7473b4813a9a98dc56a4c2fa3927e44b525f78c
Naimish Lukhi <lukhinaimish@gmail.com>
2021-06-07 12:35:47 +0530

    final project

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+let selectedColor = "white";
+let colorObject = {
+  white: [
+    "rgba(255, 255, 255, 0.973)",
+    "0px 6px rgba(210, 210, 210, 0.706)",
+    "rgb(14, 13, 13)",
+    "0px 6px rgba(210, 210, 210, 0.753)",
+    "rgb(231, 231, 231)",
+  ],
+  blue: [
+    "rgba(0, 0, 255, 0.438)",
+    "0px 6px rgba(10, 10, 134, 0.199)",
+    "rgb(0, 0, 54)",
+    "0px 6px rgba(0, 0, 54, 0.24)",
+    "rgba(15, 15, 189, 0.308)",
+  ],
+  green: [
+    "rgba(0, 128, 0, 0.712)",
+    "0px 6px rgba(6, 114, 6, 0.219)",
+    "rgb(0, 17, 0)",
+    "0px 6px rgba(0, 17, 0, 0.199)",
+    "rgba(0, 128, 0, 0.568)",
+  ],
+  red: [
+    "rgba(255, 0, 0, 0.623)",
+    "0px 6px rgba(189, 10, 10, 0.192)",
+    "rgb(44, 1, 1)",
+    "0px 6px rgba(44, 1, 1, 0.205)",
+    "rgba(201, 0, 0, 0.623)",
+  ],
+  yellow: [
+    "rgba(255, 255, 0, 0.829)",
+    "0px 6px rgba(202, 202, 5, 0.466)",
+    "rgb(160, 124, 26)",
+    "0px 6px rgba(160, 124, 26, 0.315)",
+    "rgba(226, 226, 0, 0.829)",
+  ],
+};
+
 function keyPianoHandler(event) {
   let key = document.querySelector(`span[data-key="${event.keyCode}"]`);
   playPiano(key);
@@ -12,10 +51,12 @@ let clikedKey = null;
 
 function clickPianoHandler(event) {
   clikedKey = event.target.closest("span[data-key]");
+  event.preventDefault();
   playPiano(clikedKey);
 }
 
 function clickUpHandler(event) {
+  event.preventDefault();
   focusRemove(clikedKey);
 }
 
@@ -24,7 +65,7 @@ function playPiano(key) {
     let audio = document.getElementById(key.getAttribute("data-note"));
 
     if (key.getAttribute("class") === "white-key") {
-      key.style.backgroundColor = "#e7e7e7";
+      key.style.backgroundColor = colorObject[selectedColor][4];
       key.style.boxShadow = "none";
       key.style.top = "3px";
     } else {
@@ -41,11 +82,11 @@ function playPiano(key) {
 function focusRemove(key) {
   if (key !== null) {
     if (key.getAttribute("class") === "white-key") {
-      key.style.backgroundColor = "white";
-      key.style.boxShadow = "0px 6px #d2d2d2b4";
+      key.style.backgroundColor = colorObject[selectedColor][0];
+      key.style.boxShadow = colorObject[selectedColor][1];
       key.style.top = "0px";
     } else {
-      key.style.boxShadow = "0px 6px #d2d2d2c0";
+      key.style.boxShadow = colorObject[selectedColor][3];
       key.style.top = "0px";
     }
   }
@@ -59,8 +100,6 @@ document.addEventListener("mouseup", clickUpHandler);
 
 document.addEventListener("touchstart", clickPianoHandler);
 document.addEventListener("touchend", clickUpHandler);
-
-let selectedColor = "white";
 
 function colorChangeHandler(event) {
   if (event.target.classList.contains("color-name")) {
@@ -86,51 +125,15 @@ function colorChangeHandler(event) {
 function changeColor(color) {
   let whiteKey = document.querySelectorAll(".white-key");
   let blackKey = document.querySelectorAll(".black-key");
-  if (color === "white") {
-    for (let i = 0; i < whiteKey.length; i++) {
-      whiteKey[i].style.backgroundImage = "none";
-      whiteKey[i].style.boxShadow = "0px 6px #d2d2d2b4";
-      if (i < 15) {
-        blackKey[i].style.boxShadow = "0px 6px #d2d2d2c0";
-      }
-    }
-  } else if (color === "blue") {
-    for (let i = 0; i < whiteKey.length; i++) {
-      whiteKey[i].style.backgroundImage =
-        "linear-gradient(to top,rgba(11, 11, 138, 0.747),rgba(237, 146, 255, 0.658))";
-      whiteKey[i].style.boxShadow = "0px 6px #b175ffb4";
-      if (i < 15) {
-        blackKey[i].style.boxShadow = "0px 6px #5d52f5c0";
-      }
-    }
-  } else if (color === "red") {
-    for (let i = 0; i < whiteKey.length; i++) {
-      whiteKey[i].style.backgroundImage =
-        "linear-gradient(to top,rgba(114, 12, 12, 0.849),rgba(247, 155, 155, 0.658))";
-      whiteKey[i].style.boxShadow = "0px 6px #ec7272b4";
-      if (i < 15) {
-        blackKey[i].style.boxShadow = "0px 6px #a13e3ec0";
-      }
-    }
-  } else if (color === "green") {
-    for (let i = 0; i < whiteKey.length; i++) {
-      whiteKey[i].style.backgroundImage =
-        "linear-gradient(to top,rgb(5, 112, 5),rgba(129, 231, 125, 0.712))";
-      whiteKey[i].style.boxShadow = "0px 6px rgba(93, 224, 93, 0.815)";
-      if (i < 15) {
-        blackKey[i].style.boxShadow = "0px 6px #0381038c";
-      }
-    }
-  } else {
-    for (let i = 0; i < whiteKey.length; i++) {
-      whiteKey[i].style.backgroundImage =
-        "linear-gradient(to top, yellow, rgba(231, 245, 43, 0.712))";
-      whiteKey[i].style.boxShadow = "0px 6px rgba(224, 222, 93, 0.815)";
-      if (i < 15) {
-        blackKey[i].style.boxShadow = "0px 6px #bdc00a8c";
-      }
+  for (let i = 0; i < whiteKey.length; i++) {
+    whiteKey[i].style.backgroundColor = colorObject[color][0];
+    whiteKey[i].style.boxShadow = colorObject[color][1];
+    if (i < 15) {
+      blackKey[i].style.backgroundColor = colorObject[color][2];
+      blackKey[i].style.boxShadow = colorObject[color][3];
     }
   }
 }
 
 document.addEventListener("click", colorChangeHandler);
+document.addEventListener("touchend", colorChangeHandler);
